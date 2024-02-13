111f8cd4b7dff13db905c31c790b1cef166647d9
Milan G <dev@milangress.de>
2024-02-13 20:21:17 +0100

    title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,10 +9,4 @@
 
 </script>
 
-<svelte:head>
-	<title>Radio Kutululu</title>
-	<meta name="description" content="14.02.2024 13:00 till 17:00 live from National Gallery of Prague’s Trade Fair Palace by the Sandberg Design Department" />
-	<html lang="en" />
-</svelte:head>
-
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 
+<svelte:head>
+	<title>Radio Kutululu</title>
+	<meta name="description" content="14.02.2024 13:00 till 17:00 live from National Gallery of Prague’s Trade Fair Palace by the Sandberg Design Department" />
+</svelte:head>
+
 <div class="logo">
 <h1>Radio&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kutululu</h1>
 <p>radio coming soon</p>
