957f43ec8e8abc3cb8862c97279a9bdb4a13fc83
Milan G <dev@milangress.de>
2024-02-13 19:54:36 +0100

    speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,11 @@
 	import '../app.css'
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
+
 </script>
 
 <slot />
